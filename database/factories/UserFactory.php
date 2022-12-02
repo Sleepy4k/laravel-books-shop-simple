b7a0fbc3b9959f14241e9da1c6ae476afae70b3c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-02 21:00:46 +0700

    chore: user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,6 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
             'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' // password
